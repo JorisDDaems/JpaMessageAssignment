7bbb57f493945e3ed382a80ab95e83f4bde1401b
JorisDDaems <jorisddaems@gmail.com>
2020-10-12 13:36:33 +0200

    writing to and reading from a db with jpa

---
+++ src/main/java/be/intecbrussel/SaveMessage.java
@@ -0,0 +1,34 @@
+package be.intecbrussel;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class SaveMessage {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        try{
+            emf = Persistence.createEntityManagerFactory("mypersistenceunit");
+            em = emf.createEntityManager();
+
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            Message message = new Message(1, "Hello World!");
+            em.persist(message);
+            tx.commit();
+            System.out.println("Message saved");
+        }
+        finally {
+            if (em != null){
+                em.close();
+            }
+            if (emf != null){
+                emf.close();
+            }
+        }
+    }
+}
